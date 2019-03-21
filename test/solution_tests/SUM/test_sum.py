63a292d1ea878f2952671d086882910572331e2f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-21 22:03:16 +0000

    Thu Mar 21 22:03:16 GMT 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,5 +1,5 @@
 from solutions.SUM import sum_solution
-from pytest import mark
+from pytest import mark, raises
 
 @mark.parametrize("x,y,expected", [
     (1, 2, 3),
@@ -9,6 +9,17 @@ from pytest import mark
 ])
 def test_sum_positive_cases(x, y, expected):
     assert sum_solution.compute(x, y) == expected
+
+@mark.parametrize("x,y", [
+    (-1, 2),
+    (0, -3),
+    (1, 101),
+    ("sup", ""),
+    (None, None)
+])
+def test_sum_negative_cases(x, y):
+    with raises(Exception):
+        sum_solution.compute(x, y)
 
 
 
