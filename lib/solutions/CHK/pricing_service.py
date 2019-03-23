fd2368f79974632a922f405f14d5f4f678790c5f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-23 14:57:41 +0000

    Sat Mar 23 14:57:41 GMT 2019

--- lib/solutions/CHK/pricing_service.py
+++ lib/solutions/CHK/pricing_service.py
@@ -11,21 +11,22 @@ class PricingService(object):
         char_groups = groupby(basket_string, lambda key: key)
 
         for group_key, group in char_groups:
-            sku_quantities[group_key] = group
+            sku_quantities[group_key] = len(list(group))
 
         for sku, quantity in sku_quantities.items():
             sku_info = self.sku_service.get_sku(sku)
 
             if quantity > 1 and 'offers' in sku_info and len(sku_info['offers']) > 0:
-                # relevant_offers being a generator, this is computed at call time, not assignment time
-                relevant_offers = (o for offer in sku_info['offers'] if o['quantity'] <= quantity)
+                relevant_offers = [offer for offer in sku_info['offers'] if offer['quantity'] <= quantity]
                 
                 while quantity > 1 and len(relevant_offers) > 0:
                     sorted_offers = sorted(relevant_offers, lambda key: key['quantity'], reverse=True)
                     offer = sorted_offers[0]
                     quantity -= offer['quantity']
                     total += offer['price']
+                    relevant_offers = [offer for offer in sku_info['offers'] if offer['quantity'] <= quantity]
                 
             total += sku_info['price'] * quantity
 
         return total
+
