067125674ccae6eb348f02b4ed52210fcfb16e0a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-23 13:45:37 +0000

    Sat Mar 23 13:45:37 GMT 2019

--- test/solution_tests/chk/test_checkout.py
+++ test/solution_tests/chk/test_checkout.py
@@ -2,11 +2,12 @@ from solutions.CHK.checkout_solution import checkout
 from solutions.CHK.sku_service import SkuService
 from solutions.CHK.validation_service import ValidationService
 from unittest.mock import patch
+from unittest import TestCase
 from pytest import mark
 
-@patch('validation_service.ValidationService', spec=ValidationService)
-@patch('sku_service.SkuService', spec=SkuService)
-class CheckoutTestCases():
+class CheckoutTestCases(TestCase):
+    @patch('validation_service.ValidationService', spec=ValidationService)
+    @patch('sku_service.SkuService', spec=SkuService)
     def test_checkout_unable_to_load_sku_returns_fail(self, mock_sku_service, mock_validation_service):
         mock_sku_service.load_from_json_file.side_effect = OverflowError()
 
@@ -14,6 +15,8 @@ class CheckoutTestCases():
 
         assert output == -1
 
+    @patch('validation_service.ValidationService', spec=ValidationService)
+    @patch('sku_service.SkuService', spec=SkuService)
     def test_checkout_invalid_string_returns_fail(self, mock_sku_service, mock_validation_service):
         mock_validation_service.validate_basket.return_value = False
 
