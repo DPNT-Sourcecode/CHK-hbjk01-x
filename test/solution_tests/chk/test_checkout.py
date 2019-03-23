27ef8225ef91db0ade289555e5d638b17c56579c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-23 13:48:40 +0000

    Sat Mar 23 13:48:40 GMT 2019

--- test/solution_tests/chk/test_checkout.py
+++ test/solution_tests/chk/test_checkout.py
@@ -2,24 +2,22 @@ from solutions.CHK.checkout_solution import checkout
 from solutions.CHK.sku_service import SkuService
 from solutions.CHK.validation_service import ValidationService
 from unittest.mock import patch
-from unittest import TestCase
 from pytest import mark
 
-class CheckoutTestCases(TestCase):
-    @patch('validation_service.ValidationService', spec=ValidationService)
-    @patch('sku_service.SkuService', spec=SkuService)
-    def test_checkout_unable_to_load_sku_returns_fail(self, mock_sku_service, mock_validation_service):
-        mock_sku_service.load_from_json_file.side_effect = OverflowError()
+@patch('solutions.CHK.validation_service.ValidationService', spec=ValidationService)
+@patch('solutions.CHK.sku_service.SkuService', spec=SkuService)
+def test_checkout_unable_to_load_sku_returns_fail(mock_sku_service, mock_validation_service):
+    mock_sku_service.load_from_json_file.side_effect = OverflowError()
 
-        output = checkout("stuff")
+    output = checkout("stuff")
 
-        assert output == -1
+    assert output == -1
 
-    @patch('validation_service.ValidationService', spec=ValidationService)
-    @patch('sku_service.SkuService', spec=SkuService)
-    def test_checkout_invalid_string_returns_fail(self, mock_sku_service, mock_validation_service):
-        mock_validation_service.validate_basket.return_value = False
+@patch('solutions.CHK.validation_service.ValidationService', spec=ValidationService)
+@patch('solutions.CHK.sku_service.SkuService', spec=SkuService)
+def test_checkout_invalid_string_returns_fail(mock_sku_service, mock_validation_service):
+    mock_validation_service.validate_basket.return_value = False
 
-        output = checkout("things")
+    output = checkout("things")
 
-        assert output == -1
+    assert output == -1
