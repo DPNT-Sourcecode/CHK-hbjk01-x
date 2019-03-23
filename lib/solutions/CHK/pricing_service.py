25fe8c4225101c61c93859e3e5aa06c5daaaf9a6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-23 14:21:38 +0000

    Sat Mar 23 14:21:38 GMT 2019

--- lib/solutions/CHK/pricing_service.py
+++ lib/solutions/CHK/pricing_service.py
@@ -1,6 +1,22 @@
+from itertools import groupby
+
 class PricingService(object):
     def __init__(self, sku_service):
         self.sku_service = sku_service
 
     def get_price(self, basket_string):
-        return 0
+        total = 0
+        sku_quantities = {}
+        
+        char_groups = groupby(basket_string, lambda key: key)
+
+        for group_key, group in char_groups:
+            sku_quantities[group_key] = len(group)
+
+        for sku, quantity in sku_quantities.items():
+            sku_info = self.sku_service.get_sku(sku)
+
+            if 'offers' in sku_info and len(sku_info['offers']) > 0:
+                pass
+
+        return total
